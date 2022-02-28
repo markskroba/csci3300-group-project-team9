bab63a534900cad2f1041d038267e315f092ffdd
Mark Skroba <mark.skroba@slu.edu>
2022-02-28 10:22:44 -0600

    fixed input always being wrong because of type mismatch

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ Main file
 while True:
     i = input("Enter: \n1 to add a question\n2 to view added questions\nQ to exit: ")
 
-    if i == 1:
+    if i == "1":
         # logic for adding a question
         pass
-    elif i == 2:
+    elif i == "2":
         # logic for printing all questions
         pass
     elif i == "Q":
