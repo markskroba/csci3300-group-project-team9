a2771de951639f8a4b28965dab1ec719f8a4c512
Mark Skroba <mark.skroba@slu.edu>
2022-03-04 09:15:21 -0600

    adding a question controller

---
+++ controllers/question_controller.py
@@ -0,0 +1,16 @@
+'''Controller'''
+from models.multiple_choice_question import *
+import json
+
+class QuestionController():
+    '''Controllers class for questions'''
+    def __init__(self):
+        self.questions = []
+
+    def add_question(self):
+        '''Adding a question'''
+        pass
+
+    def print_questions(self):
+        '''Printing all questions'''
+        pass
