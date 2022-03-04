33c820835a4a63ba3f4784de460b20c3b4e479ff
Samuel T. Bounds <boundss@hopper3.slu.edu>
2022-03-04 10:37:38 -0600

    Some fixes for pyline

--- submit_question.py
+++ submit_question.py
@@ -1,9 +1,17 @@
+'''
+This takes in a question and saves the information to a text file.
+Information is seperated by commas and new lines.
 
+Input:
+question   - string representation of question
+answers    - array of strings representing answers
+properties - array of strings representing properties
+'''
 def submit(question, answers, properties):
-	file = open("database.txt", "a")
-	file.write(question + "\n")
-	file.write(",".join(answers) + "\n")
-	file.write(",".join(properties) + "\n")
-	file.write("\n")
-	file.close()
-
+    '''Opens a file, appends information, closes the file '''
+    file = open("database.txt", "a")
+    file.write(question + "\n")
+    file.write(",".join(answers) + "\n")
+    file.write(",".join(properties) + "\n")
+    file.write("\n")
+    file.close()
