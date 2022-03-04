9be8d66ba6b8cc6f749cb0773056fef817a356d0
natalie-pullen <98914351+natalie-pullen@users.noreply.github.com>
2022-03-03 21:52:15 -0600

    fixing pylint issues

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ while True:
     i = input("Enter: \n1 to add a question\n2 to view added questions\nQ to exit: ")
 
     if i == "1":
-        questionType = input("Enter: \n1 for Multiple choice\n2 for Fill in the blank\n3 for Short answer or \n4 for Essay: ")
+        print("Enter: \n1 for Multiple choice\n2 for Fill in the blank\n3 for Short answer or \n4 for Essay: ")
+        questionType = input()
         if questionType == "1":
             # logic for multiple choice question
             pass
@@ -20,7 +21,6 @@ while True:
             pass
         else:
             print("Wrong input")
-        pass
     elif i == "2":
         # logic for printing all questions
         pass
