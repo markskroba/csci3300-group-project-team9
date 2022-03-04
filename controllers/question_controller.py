5112ce2cb5019696eb0ecfd35071a10a46e9a698
Mark Skroba <mark.skroba@slu.edu>
2022-03-04 09:22:47 -0600

    more pylint fixes

--- controllers/question_controller.py
+++ controllers/question_controller.py
@@ -4,10 +4,12 @@ class QuestionController():
     def __init__(self):
         self.questions = []
 
-    def add_question(self):
+    def add_question(self, question):
         '''Adding a question'''
+        self.questions.append(question)
         print("adding a question")
 
     def print_questions(self):
         '''Printing all questions'''
+        print(self.questions)
         print("printing all questions")
