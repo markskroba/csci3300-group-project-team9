fad55ef36e1a553e52f747289bc8d3c9f994ce26
Mark Skroba <mark.skroba@slu.edu>
2022-03-08 15:22:00 -0600

    adding a model for fill in the blank question

---
+++ models/fill_in_question.py
@@ -0,0 +1,19 @@
+'''Model for fill in the blank question'''
+from models.question import Question
+
+class FillInQuestion(Question):
+    '''
+    Class for fill in the blank question
+    body: String where "blanks" are denoted by multiple underscores '_'
+    answers: List of strings of answers that correspond to blanks
+    '''
+    def __init__(self, body, when_used, difficulty, answers):
+        super().__init__(body, when_used, difficulty)
+        self.answers = answers
+
+    def print(self):
+        '''Prints the question (for non-GUI version)'''
+        super().print()
+        print("Question type: Fill in the blank question")
+        print(f'Answers: {", ".join(self.answers)}')
+        
