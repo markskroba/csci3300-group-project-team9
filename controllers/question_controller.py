06450b5fe4c9ca180885d37e98657348dcbb9035
Mark Skroba <mark.skroba@slu.edu>
2022-03-10 15:07:30 -0600

    printing table with questions using prettytable and questions individually on user input

--- controllers/question_controller.py
+++ controllers/question_controller.py
@@ -1,8 +1,12 @@
 '''Controller'''
+from prettytable import PrettyTable
+
+from question_database_json import QuestionDatabaseJSON
+
 class QuestionController():
     '''Controllers class for questions'''
     def __init__(self):
-        self.questions = []
+        self.db = QuestionDatabaseJSON("data.json")
 
     def add_question(self):
         '''Adding a question'''
@@ -11,5 +15,18 @@ class QuestionController():
 
     def print_questions(self):
         '''Printing all questions'''
-        print(self.questions)
-        print("printing all questions")
+        table = PrettyTable(["Number", "Question Body", "Question Type"])
+        for count, question in enumerate(self.db.questions):
+            table.add_row([count + 1, question.body, type(question).__name__])
+        print(table)
+        while True:
+            try: 
+                i = input("Enter:\n0 to go back\nQuestion number to print that question: ")
+                if int(i) == 0:
+                    break
+                elif int(i) <= len(self.db.questions) + 1:
+                    self.db.questions[int(i) - 1].print()
+                else:
+                    print("Wrong input")
+            except ValueError():
+                print("Wrong input")
