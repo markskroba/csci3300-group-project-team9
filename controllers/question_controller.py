1f7cec1015dca436b0a0e6fcabf1b1d3e8d2a96d
Mark Skroba <mark.skroba@slu.edu>
2022-03-10 15:21:52 -0600

    pylint fixes

--- controllers/question_controller.py
+++ controllers/question_controller.py
@@ -6,7 +6,8 @@ from question_database_json import QuestionDatabaseJSON
 class QuestionController():
     '''Controllers class for questions'''
     def __init__(self):
-        self.db = QuestionDatabaseJSON("data.json")
+        self.questions = []
+        self.database = QuestionDatabaseJSON("data.json")
 
     def add_question(self):
         '''Adding a question'''
@@ -16,16 +17,16 @@ class QuestionController():
     def print_questions(self):
         '''Printing all questions'''
         table = PrettyTable(["Number", "Question Body", "Question Type"])
-        for count, question in enumerate(self.db.questions):
+        for count, question in enumerate(self.database.questions):
             table.add_row([count + 1, question.body, type(question).__name__])
         print(table)
         while True:
-            try: 
+            try:
                 i = input("Enter:\n0 to go back\nQuestion number to print that question: ")
-                if int(i) == 0:
+                if int(i) <= len(self.database.questions) + 1 and int(i) > 0:
+                    self.database.questions[int(i) - 1].print()
+                elif int(i) == 0:
                     break
-                elif int(i) <= len(self.db.questions) + 1:
-                    self.db.questions[int(i) - 1].print()
                 else:
                     print("Wrong input")
             except ValueError():
