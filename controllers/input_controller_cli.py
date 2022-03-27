d8909635da0aa261488d5b3114485423be1a6a63
Mark Skroba <mark.skroba@slu.edu>
2022-03-27 17:19:28 -0500

    creating new class, moving input to it

---
+++ controllers/input_controller_cli.py
@@ -0,0 +1,111 @@
+'''Controller'''
+from datetime import datetime
+
+class IOControllerCLI():
+    '''Class for taking input from CLI'''
+
+    def get_question_type(self):
+        '''Get a question type to determine what type-specific arguments will be needed'''
+        question_type = input("""Select question type:
+1 for multiple choice question
+2 for fill in the blank question
+3 for short answer question
+0 to quit: """)
+
+        return question_type
+
+    def get_body(self):
+        '''Get body of a question'''
+        i = input("Enter your question: ")
+        return i
+
+    def get_dates(self):
+        '''Get first used and last used dates for specific question'''
+        while True:
+            try:
+                first_used_i = input("""When was this question first used?
+(month-day-year) (ENTER for now):""")
+                if first_used_i == "":
+                    first_used = int(datetime.today().timestamp())
+                else:
+                    month, day, year = first_used_i.split("-")
+                    first_used = int(datetime(int(year), int(month), int(day)).timestamp())
+
+                break
+            except ValueError:
+                print("Wrong input")
+                continue
+        while True:
+            try:
+                last_used_i = input("""When was this question last used?
+(month-day-year) (ENTER for now):""")
+                if last_used_i == "":
+                    last_used = int(datetime.today().timestamp())
+                else:
+                    month, day, year = last_used_i.split("-")
+                    last_used = int(datetime(int(year), int(month), int(day)).timestamp())
+                break
+            except ValueError:
+                print("Wrong input")
+                continue
+
+        when_used = {"first_used": first_used, "last_used": last_used}
+        return when_used
+
+    def get_difficulty(self):
+        '''Get difficulty of question'''
+        difficulty = input("Enter you question's difficulty, from 1 to 5: ")
+        return difficulty
+
+    def get_multiple_choice_answers(self):
+        '''Get answers for multiple choice question'''
+        answers = []
+        print("You will now enter answers for your multiple choice question")
+        answer_counter = 0
+        while True:
+            answer_body = input(f'Enter answer #{answer_counter}: ')
+            answer_correct = bool(input("""Is this answer true or false?
+Enter 1 for true and anything else (like 0) for false: """) == "1")
+
+            answers.append({"body": answer_body, "correct": answer_correct})
+            answer_counter += 1
+            i = input("Enter 1 if you want to add one more answer: ")
+            if i != "1":
+                break
+        return answers
+
+    def get_fill_in_answers(self):
+        '''Get answers for fill in the blank question'''
+        answers = []
+        answer_counter = 0
+        print("You will now enter answers for your fill in the blank question")
+        while True:
+            answer_body = input(f'Enter answer #{answer_counter}: ')
+            answers.append(answer_body)
+
+            i = input("Enter 1 if you want to add one more answer: ")
+            if i != "1":
+                break
+        return answers
+
+    def get_short_answer_properties(self):
+        '''Get maximum word count for an answer and key points for it'''
+        while True:
+            max_word_count = input("Enter the max word count for the answer: ")
+            try:
+                max_word_count = int(max_word_count)
+                break
+            except TypeError:
+                print("Wrong input")
+                continue
+        print("You will now enter key points that should be addressed in a correct answer")
+        key_points = []
+        while True:
+            key_point = input("""Enter one key point
+or 0 if you don't want to enter any more: """)
+            if key_point != "0":
+                key_points.append(key_point)
+            else:
+                break
+
+        return max_word_count, key_points
--- controllers/question_controller.py
+++ controllers/question_controller.py
@@ -1,5 +1,4 @@
 '''Controller'''
-from datetime import datetime
 from prettytable import PrettyTable
 
 from question_database_json import QuestionDatabaseJSON
@@ -8,122 +7,31 @@ from models.multiple_choice_question import MultipleChoiceQuestion
 from models.short_answer_question import ShortAnswerQuestion
 from models.fill_in_question import FillInQuestion
 
+from controllers.input_controller_cli import IOControllerCLI 
+
 class QuestionController():
     '''Controllers class for questions'''
     def __init__(self):
         self.database = QuestionDatabaseJSON("data.json")
-        self.methods_used = 0
-
-    def get_dates(self):
-        '''Get first used and last used dates for specific question'''
-        while True:
-            try:
-                first_used_i = input("""When was this question first used?
-(month-day-year) (ENTER for now):""")
-                if first_used_i == "":
-                    first_used = int(datetime.today().timestamp())
-                else:
-                    month, day, year = first_used_i.split("-")
-                    first_used = int(datetime(int(year), int(month), int(day)).timestamp())
-
-                break
-            except ValueError:
-                print("Wrong input")
-                continue
-        while True:
-            try:
-                last_used_i = input("""When was this question last used?
-(month-day-year) (ENTER for now):""")
-                if last_used_i == "":
-                    last_used = int(datetime.today().timestamp())
-                else:
-                    month, day, year = last_used_i.split("-")
-                    last_used = int(datetime(int(year), int(month), int(day)).timestamp())
-                break
-            except ValueError:
-                print("Wrong input")
-                continue
-
-        when_used = {"first_used": first_used, "last_used": last_used}
-        self.methods_used += 1
-        return when_used
-
-    def get_multiple_choice_answers(self):
-        '''Get answers for multiple choice question'''
-        answers = []
-        print("You will now enter answers for your multiple choice question")
-        answer_counter = 0
-        while True:
-            answer_body = input(f'Enter answer #{answer_counter}: ')
-            answer_correct = bool(input("""Is this answer true or false?
-Enter 1 for true and anything else (like 0) for false: """) == "1")
-
-            answers.append({"body": answer_body, "correct": answer_correct})
-            answer_counter += 1
-            i = input("Enter 1 if you want to add one more answer: ")
-            if i != "1":
-                break
-        self.methods_used += 1
-        return answers
-
-    def get_fill_in_answers(self):
-        '''Get answers for fill in the blank question'''
-        answers = []
-        answer_counter = 0
-        print("You will now enter answers for your fill in the blank question")
-        while True:
-            answer_body = input(f'Enter answer #{answer_counter}: ')
-            answers.append(answer_body)
-
-            i = input("Enter 1 if you want to add one more answer: ")
-            if i != "1":
-                break
-        self.methods_used += 1
-        return answers
-
-    def get_short_answer_properties(self):
-        '''Get maximum word count for an answer and key points for it'''
-        while True:
-            max_word_count = input("Enter the max word count for the answer: ")
-            try:
-                max_word_count = int(max_word_count)
-                break
-            except TypeError:
-                print("Wrong input")
-                continue
-        print("You will now enter key points that should be addressed in a correct answer")
-        key_points = []
-        while True:
-            key_point = input("""Enter one key point
-or 0 if you don't want to enter any more: """)
-            if key_point != "0":
-                key_points.append(key_point)
-            else:
-                break
-
-        self.methods_used += 1
-        return max_word_count, key_points
+        self.input_controller = IOControllerCLI()
 
     def add_question(self):
         '''Adding a question'''
 
         while True:
-            question_type_i = input("""Select question type:
-1 for multiple choice question
-2 for fill in the blank question
-3 for short answer question
-0 to quit: """)
+            question_type_i = self.input_controller.get_question_type()
+
             if question_type_i not in ["1","2","3","0"]:
                 print("Wrong input")
                 continue
             if question_type_i == "0":
                 break
-            body = input("Enter your question: ")
+            body = self.input_controller.get_body()
             # getting dates
-            when_used = self.get_dates()
+            when_used = self.input_controller.get_dates()
             # getting difficulty
             while True:
-                difficulty = input("Enter you question's difficulty, from 1 to 5: ")
+                difficulty = self.input_controller.get_difficulty()
                 if difficulty in ["1", "2", "3", "4", "5"]:
                     difficulty = int(difficulty)
                 else:
@@ -134,19 +42,19 @@ or 0 if you don't want to enter any more: """)
             # getting question arguments
             if question_type_i == "1":
                 # multiple choice
-                answers = self.get_multiple_choice_answers()
+                answers = self.input_controller.get_multiple_choice_answers()
                 question = MultipleChoiceQuestion(body, when_used, difficulty, answers)
                 self.database.submit_question(question)
 
             elif question_type_i == "2":
                 # fill in the blank
-                answers = self.get_fill_in_answers()
+                answers = self.input_controller.get_fill_in_answers()
                 question = FillInQuestion(body, when_used, difficulty, answers)
                 self.database.submit_question(question)
 
             elif question_type_i == "3":
                 # short answer
-                max_word_count, key_points = self.get_short_answer_properties()
+                max_word_count, key_points = self.input_controller.get_short_answer_properties()
                 question = ShortAnswerQuestion(body,
                 when_used,
                 difficulty,
