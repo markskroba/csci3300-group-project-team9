e078329a214762d9beb3e6ae5fcf03bcd346bebb
Mark Skroba <mark.skroba@slu.edu>
2022-04-07 16:57:38 -0500

    basic layout for adding question window

---
+++ views/add_question_screen.py
@@ -0,0 +1,43 @@
+import PySimpleGUI as sg
+import tk
+
+get_question_body = [
+    sg.Text("Question body: "),
+    sg.Multiline(size=(50, 10), key="-BODY-"),
+]
+
+get_question_difficulty = [
+    sg.Text("Question difficulty: "),
+    sg.Spin([i for i in range(1,6)], initial_value=5, k='-DIFFICULTY-'),
+]
+
+get_dates = [
+    [
+        sg.Text("Date when the question was first used (leave empty for today): "),
+        sg.CalendarButton("Date", format="%m/%d/%Y", target="-FIRSTUSED-"),
+        sg.In(size=(10, 1), enable_events=True, key="-FIRSTUSED-"),
+    ],
+    [
+        sg.Text("Date when the question was last used (leave empty for today): "),
+        sg.CalendarButton("Date", format="%m/%d/%Y", target="-LASTUSED-"),
+        sg.In(size=(10, 1), enable_events=True, key="-LASTUSED-"),
+    ]
+]
+
+submit_button = [
+    sg.Button(button_text="Submit", enable_events=True, key="-SUBMIT-")]
+
+inputs = sg.Column([get_dates[0], get_dates[1], get_question_difficulty, get_question_body, submit_button], element_justification="center")
+layout = [[inputs]]
+
+window = sg.Window("Add Question", layout)
+
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    elif event == "-SUBMIT-":
+        body = values["-BODY-"]
+        difficulty = values["-DIFFICULTY-"]
+        first_used = values["-FIRSTUSED-"]
+        last_used = values["-LASTUSED-"]
