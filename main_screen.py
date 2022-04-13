f6829cf003bd2106b05e40248b8215bf8c0507ab
Mark Skroba <mark.skroba@slu.edu>
2022-04-12 19:33:16 -0500

    replacing buttons with listbox, displaying some question properties on the left side

--- main_screen.py
+++ main_screen.py
@@ -2,8 +2,10 @@
 import string
 from subprocess import Popen
 
+from datetime import datetime
 import PySimpleGUI as sg
 from main_screen_wrapper import MainWrapper, format_answers
+from question_database_json import QuestionDatabaseJSON
 
 wrapper = MainWrapper("data.json")
 
@@ -27,17 +29,20 @@ question_list_footer = [
                         [sg.Button('Add a question'), sg.Button('Cancel')]
                     ]
 
-question_list_column = question_list_header + wrapper.create_buttons(3) + \
-     question_list_footer
+# question_list_column = question_list_header + wrapper.create_buttons(3) + \
+#      question_list_footer
+db = QuestionDatabaseJSON("data.json")
+question_titles = [q.body for q in db.questions]
+question_list_column = question_list_header + [[
+    sg.Listbox(question_titles, size=(50,5), enable_events=True, key="-QLIST-")
+]]
 
-question_body_column = [
-            [sg.Text("Question #", size=(20), font=(40), key = '-TITLE-')],
-            [sg.Text("Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit,"\
-            " sed do labore et dolore magna aliqua?", size=(30,4), key='-QUESTION-')],
-            [sg.Text("A: Lorem ipsum dolor sit amet, consectetur adipiscing elit,"\
-            " sed do labore et dolore magna aliqua.", size=(30,8), key='-ANSWER-')],
-            [sg.Text("Difficulty:???", size = (10,1), key='-Q_DIFFICULTY-')]
-]
+question_body_column = [[sg.Text("Question #", size=(20), font=(40), key = '-TITLE-')],
+            [sg.Text("", key='-QUESTION-', size=(20,2))],
+            [sg.Text("", key="-Q_FIRST_USED-")],
+            [sg.Text("", key="-Q_LAST_USED-")],
+            [sg.Text("Difficulty:???", size = (10,1), key='-Q_DIFFICULTY-')]]
+
 
 layout = [
     [
@@ -78,11 +83,23 @@ while True:
         for to_be_enabled in clean_buttons:
             window[to_be_enabled].update(visible = True)
 
+    elif event == "-QLIST-":
+        for q in db.questions:
+            if q.body == values["-QLIST-"][0]:
+                print(q)
+
+                window["-QUESTION-"].update(q.body)
+                window["-Q_DIFFICULTY-"].update(f'Difficulty: {q.difficulty}')
+                window["-Q_FIRST_USED-"].update(
+                    f'Question first added: {datetime.fromtimestamp(q.first_used).strftime("%b %d, %Y")}')
+                window["-Q_LAST_USED-"].update(
+                    f'Question first added: {datetime.fromtimestamp(q.last_used).strftime("%b %d, %Y")}')
+
+
     elif event not in('Search', 'Add a question', '-LASTUSED-', '-FIRSTUSED-'):
         details = wrapper.get_details(event.rstrip(string.digits))
         window["-TITLE-"].update(details[0])
         window["-QUESTION-"].update(details[0])
-        window["-ANSWER-"].update(format_answers(details[1]))
         window["-Q_DIFFICULTY-"].update("Difficulty: " + str(details[2]))
     if PROCESS != -1 and PROCESS.poll() is not None:
         OPEN_THREAD = False
