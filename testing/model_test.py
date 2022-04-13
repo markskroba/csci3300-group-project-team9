cfda4cf49ce329d61a014ff1f927d8887e12437e
natalie-pullen <98914351+natalie-pullen@users.noreply.github.com>
2022-04-12 20:56:26 -0500

    pylint fixes

--- testing/model_test.py
+++ testing/model_test.py
@@ -7,9 +7,9 @@ from models.short_answer_question import ShortAnswerQuestion
 
 def test_fill_in_basics():
     '''tests that when initiating a fill in the blank question, all properties are set correctly'''
-    question = FillInQuestion("This is a ___ question", {"first_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s'),
-                                                         "last_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s')},
-                              1, ["fill in the blank"])
+    dates = {"first_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s'),
+             "last_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s')}
+    question = FillInQuestion("This is a ___ question", dates, 1, ["fill in the blank"])
     assert question.body == "This is a ___ question"
     assert question.first_used == "1625637721"
     assert question.last_used == "1625637721"
@@ -18,9 +18,9 @@ def test_fill_in_basics():
 
 def test_multiple_choice():
     '''tests that when initiating a multiple choice question, all properties are set correctly'''
-    question = MultipleChoiceQuestion("Choose the best answer:",
-                                      {"first_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s'),
-                                       "last_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s')}, 3,
+    dates = {"first_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s'),
+             "last_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s')}
+    question = MultipleChoiceQuestion("Choose the best answer:", dates, 3,
                                       ["first option", "second option", "third option"])
     assert question.body == "Choose the best answer:"
     assert question.first_used == "1625637721"
@@ -30,9 +30,9 @@ def test_multiple_choice():
 
 def test_short_answer():
     '''tests that when initiating a short answer question, all properties are set correctly'''
-    question = ShortAnswerQuestion("This is a short answer question", {"first_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s'),
-                                                         "last_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s')},
-                              4, {"max_word_count":350, "key_points": ["point 1", "point 2", "point 3"]})
+    dates = {"first_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s'), "last_used": datetime(2021, 7, 7, 1, 2, 1).strftime('%s')}
+    question = ShortAnswerQuestion("This is a short answer question", dates,
+                                   4, {"max_word_count":350, "key_points": ["point 1", "point 2", "point 3"]})
     assert question.body == "This is a short answer question"
     assert question.first_used == "1625637721"
     assert question.last_used == "1625637721"
