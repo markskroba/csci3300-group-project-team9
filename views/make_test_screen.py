d646b230582dd9c93a363a49ee0b837b9cc64574
Mark Skroba <mark.skroba@slu.edu>
2022-05-04 19:19:51 -0500

    adding ui, no functionality so far

--- views/make_test_screen.py
+++ views/make_test_screen.py
@@ -1,7 +1,33 @@
 '''Window for making tests out of saved questions'''
 import PySimpleGUI as sg
 
-layout = [[sg.Text("Window for making new tests")]]
+note = [
+    [sg.Text("NOTE: Make new test")],
+    [sg.Text(
+"""Pick questions that you previously saved to be a part of this new test.
+Before adding, you will be able to preview them""",
+size=(40, 3))],
+    [
+        sg.Button("Generate PDF"),
+        sg.Button("Clear selection")
+    ]
+]
+
+header_preview = [
+        [sg.Text("Question body")],
+        [sg.Text("Question type")],
+        [sg.Text("Question difficulty")]
+]
+
+# header = [header_text, header_preview]
+
+listboxes = [
+    sg.Listbox(["test1", "test2"], size=(50,10), enable_events=True, key="-SAVEDQUESTIONS-"),
+    sg.Listbox(["test1", "test2"], size=(50,10), enable_events=True, key="-SELECTEDQUESTIONS-"),
+]
+
+layout = [[header_preview, listboxes, note ]]
+
 window = sg.Window("Make test", layout)
 
 while True:
