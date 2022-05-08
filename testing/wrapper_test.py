0c2f9207c4ed29441d30693b53c8eb35ac934388
Sam Bounds <samuel.bounds@slu.edu>
2022-05-08 13:41:42 -0500

    fixes for pylint?

--- testing/wrapper_test.py
+++ testing/wrapper_test.py
@@ -1,30 +1,35 @@
+'''Testing main_screen_wrapper.py, which is the functions that main_screen uses'''
 from main_screen_wrapper import MainWrapper
 
 wrapper = MainWrapper("test_data.json")
 question_list = []
-for item in wrapper.database.questions:
-    question_list.append(item)
+for question in wrapper.database.questions:
+    question_list.append(question)
 
 def test_get_details_false():
+    '''Asserts that a question that doesnt exist doesnt have any details returned'''
     question = "Not in list"
     result = wrapper.get_details(question)
-    assert result == []
+    assert result is []
 
 def test_get_details_true():
+    '''Asserts that a question that does exist has proper details returned'''
     question = "What?"
     result = wrapper.get_details(question)
     assert result == ['What?', 'True: Answer 1!\nFalse: incorrect '\
         'answer\nFalse: other incorrect answer\n', 4, 'Multiple Choice']
 
 def test_create_buttons_size():
+    '''Asserts that the 2D array of buttons is the right dimensions and non-empty'''
     row_size = 3
     result = wrapper.create_buttons(row_size)
-    assert result != []
+    assert result is not []
     assert len(result[0]) == 3
     for item in result:
         assert len(item) <= row_size
 
 def test_filtered_buttons_type():
+    '''Asserts that the filtering of questions by type works correctly'''
     criteria = ['Short Answer','','','','']
     filtered,clean = wrapper.filtered_buttons(criteria)
     for item in question_list:
@@ -36,6 +41,7 @@ def test_filtered_buttons_type():
             assert item.type != 'Short Answer'
 
 def test_filtered_buttons_difficulty():
+    '''Asserts that the filtering of questions by difficulty works correctly'''
     criteria = ['','5','','','']
     filtered,clean = wrapper.filtered_buttons(criteria)
     for item in question_list:
